ace0597bb32f78b96f9dd6fbd77053fa56c5ed81
jang48 <jjangmi4843@gmail.com>
2023-09-11 10:16:15 +0900

    기능3 : 게시물 조회

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<String> boards = new ArrayList<>();
+        int boardcnt = 0;
 
-        while(true){
+        while (true) {
             System.out.print("메뉴 입력 : ");
             Scanner sc = new Scanner(System.in);
 
             String func = sc.nextLine().trim();
 
-            if(func.equals("a")){
+            if (func.equals("a")) {
                 System.out.println("기능1");
-            }else if(func.equals("b")){
+            } else if (func.equals("b")) {
                 System.out.println("기능2");
-            }else if(func.equals("도움말")) {
+            } else if (func.equals("도움말")) {
                 System.out.println("a : 기능1");
                 System.out.println("b : 기능1");
-            }else if(func.equals("add")){
+            } else if (func.equals("add")) {
                 System.out.print("게시물 제목을 입력해주세요 : ");
                 String title = sc.nextLine();
                 System.out.print("게시물 내용을 입력해주세요 : ");
                 String story = sc.nextLine();
                 System.out.println("게시물이 등록되었습니다.");
-            }else if (func.equals("exit")){
+                boardcnt++;
+                String board = "==================\n" + " 제목: " + title + "\n" + " 내용: " + story + "\n";
+                boards.add(board); // 게시물을 리스트에 추가
+            } else if (func.equals("list")) {
+                for (String board : boards) {
+                    System.out.println(board);
+                }
+            } else if (func.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
             }
