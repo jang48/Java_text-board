2deacf54dadd2a2a010f95c1d49025adcdeb3015
jang48 <jjangmi4843@gmail.com>
2023-09-11 11:10:34 +0900

    기능4 : 게시물 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,34 +4,57 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 이름 짓는 규칙
+        // 변수, 클래스 -> 명사
+        // 함수 -> 동사
+        // 단어 조합시 단어 의미가 바뀔 때마다 대문자 표현  - > 카멜표기법
+        // 클래스 이름 지을 때는 첫글자 무조건 대문자
+        // 누가 봐도 추측 가능한.
+        // 파파고나 구글 번역기, 챗 gpt 켜서 이름 짓기
+        // 이름 짓는 건 너무너무너무너무 중요함
+
+        // 코드 정렬
+        // Alt + Ctrl + L  ( 자동 줄맞춤)
+
+        Scanner sc = new Scanner(System.in);
         List<String> boards = new ArrayList<>();
         int boardcnt = 0;
 
         while (true) {
-            System.out.print("메뉴 입력 : ");
-            Scanner sc = new Scanner(System.in);
-
+            System.out.print("명령어 : ");
             String func = sc.nextLine().trim();
 
-            if (func.equals("a")) {
-                System.out.println("기능1");
-            } else if (func.equals("b")) {
-                System.out.println("기능2");
-            } else if (func.equals("도움말")) {
-                System.out.println("a : 기능1");
-                System.out.println("b : 기능1");
-            } else if (func.equals("add")) {
+            if (func.equals("add")) {
                 System.out.print("게시물 제목을 입력해주세요 : ");
                 String title = sc.nextLine();
                 System.out.print("게시물 내용을 입력해주세요 : ");
                 String story = sc.nextLine();
                 System.out.println("게시물이 등록되었습니다.");
+
                 boardcnt++;
-                String board = "==================\n" + " 제목: " + title + "\n" + " 내용: " + story + "\n";
-                boards.add(board); // 게시물을 리스트에 추가
+
+                String board = "==================\n" + " 번호 : " + boardcnt + "\n"+" 제목: " + title ;
+                boards.add(boardcnt-1,board);
             } else if (func.equals("list")) {
+                int index = 1;
                 for (String board : boards) {
+                    index++;
                     System.out.println(board);
+                }
+            } else if (func.equals("update")) {
+                System.out.print("수정할 게시물 번호 : ");
+                int postNumber = Integer.parseInt(sc.nextLine()) - 1;
+                if(postNumber >= 0 && postNumber < boards.size()) {
+                    System.out.print("제목 : ");
+                    String updatedTitle = sc.nextLine();
+                    System.out.print("내용 : ");
+                    String updatedStory = sc.nextLine();
+                    System.out.println((postNumber + 1) + " 번 게시물이 수정되었습니다.");
+
+                    String updatedBoard = "==================\n" + " 번호 : " + (postNumber + 1) +"\n" + " 제목: " + updatedTitle;
+                    boards.set(postNumber, updatedBoard);
+                } else {
+                    System.out.println("없는 게시물 번호입니다.");
                 }
             } else if (func.equals("exit")) {
                 System.out.println("프로그램을 종료합니다.");
