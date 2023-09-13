e16d04efa66cabf986c9fa9e84d646853cbb7382
jang48 <jjangmi4843@gmail.com>
2023-09-13 14:19:22 +0900

    기능12 : 댓글 기능 구현 완료

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -91,23 +91,29 @@ public class Main {
                 System.out.println("조회수 : " + article.getClick());
                 System.out.println("========================");
 
-                for(int i = 0; i < plus.size(); i++){
-                    Plus plus1 = plus.get(i);
-                    System.out.printf("번호 : %d\n", i + 1);
-                    System.out.printf("제목 : %s\n",article.getTitle());
-                    System.out.println("===================");
+                if(!plus.isEmpty()){
+                    //lus plus1 = plus.get(targetId-1);
+                    for (Plus plus1 : plus) {
+                        if(plus1.getId() == targetId){
+                            System.out.printf("댓글 내용 : %s\n", plus1.getWrite());
+                            System.out.printf("댓글 작성일 : %s\n",plus1.getDate());
+                            System.out.println("===================");
+                        }
+                    }
+
                 }
 
                 System.out.print("상세보기 기능을 선택해주세요(1. 댓글 등록, 2. 추천, 3. 수정, 4. 삭제, 5. 목록으로) : ");
                 int function = sc.nextInt();
-                sc.nextLine();
-                System.out.print("댓글 내용 : ");
-                String comment = sc.nextLine();
-                Plus newplus = new Plus(targetId, comment, formatedNow);
-                plus.add(newplus);
-                System.out.print("댓글이 성공적으로 등록되었습니다\n");
 
-
+                if(function == 1){
+                    sc.nextLine();
+                    System.out.print("댓글 내용 : ");
+                    String comment = sc.nextLine();
+                    Plus newplus = new Plus(targetId, comment, formatedNow);
+                    plus.add(newplus);
+                    System.out.print("댓글이 성공적으로 등록되었습니다\n");
+                }
 
 
             } else if (command.equals("search")) {
